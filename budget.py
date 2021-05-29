15bdd69e8ea395e7851a8a074828170c24f1204d
Nahuel <>
2021-05-29 15:42:19 -0300

    budget finished

--- budget.py
+++ budget.py
@@ -3,10 +3,14 @@ class Category:
     Category = ''
     ledger = list()
     funds = 0
+    total_withdraw = 0
 
 
     def __init__(self, name):
         self.name = name
+        self.ledger = list()
+        self.funds = 0
+        self.total_withdraw = 0
 
     def __str__(self):
         mask_t = '******************************' # 30 *
@@ -68,6 +72,7 @@ class Category:
             d['description'] = description
             self.ledger.append(d)
             self.funds -= amount
+            self.total_withdraw += amount
             return True
         return False
         
@@ -78,15 +83,72 @@ class Category:
         w = self.withdraw(amount, description)
 
         description = 'Transfer from ' + self.name
-        if(w): category.deposit(amount)
+        if(w): category.deposit(amount, description)
 
         return w
         
-
+    def get_total_withdraw(self):
+        return self.total_withdraw
 
     
-def create_spend_chart(l):
-    return 0;
+def create_spend_chart(categories):
+    
+    total = 0
+    percentages = []
+    names = []
+    max_len = 0
+    enter = '\n'
+    res = 'Percentage spent by category\n'
+
+    for i in range(len(categories)):
+        total += categories[i].get_total_withdraw()
+    
+    for i in range(len(categories)):
+        percentages.append((categories[i].get_total_withdraw() * 100) // total)
+        names.append(categories[i].name.capitalize())
+        max_len = max(max_len, len(names[i]))
+
+    # percentages bar
+    p = 100
+    while (p >= 0):
+        if p == 100: res += '100| '
+        elif p == 0: res += '  0| '
+        else: res += ' '+ str(p) + '| '
+
+        for i in range(len(categories)):
+            if percentages[i] >= p : res += 'o  '
+            else: res += '   '
+        p-= 10
+        res += enter
+    
+    # line -----...
+    res += '    -'
+    for i in range(0, len(categories)):
+        res+= '---'
+    res += enter
+
+    # format names
+    mask_name = ''
+    for i in range(0,max_len):
+        mask_name += ' '
+        
+    for i in range(0, len(names)):
+        n = names[i]
+        if len(names[i]) < max_len: names[i] = n + mask_name[len(n):len(mask_name)] + '.'
+
+
+    # names
+    for i in range(0, max_len):
+        res += '     '
+        for j in range(0, len(categories)):
+            res += names[j][i] + '  '
+        res += enter
+    
+    return res
+
+
+
+
 
 
 food = Category("Food")
@@ -105,5 +167,4 @@ auto.withdraw(15)
 print(food)
 print(clothing)
 
-    
-
+print(create_spend_chart([food, clothing, auto]))
