22cbee89c81dcd7d17a21c42e4ef104d3b7cede8
Nahuel <>
2021-05-29 03:11:23 -0300

    arithmetic_formatter finished

--- arithmeticFormatter.py
+++ arithmeticFormatter.py
@@ -1,13 +1,3 @@
-# arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-
-#   32         1      9999      523
-# +  8    - 3801    + 9999    -  49
-# ----    ------    ------    -----
-#   40     -3800     19998      474
-
-
-# code submitted in:
-# https://replit.com/@nahuelcastro/boilerplate-arithmetic-formatter#arithmetic_arranger.py
 def arithmetic_arranger(problems, b = False):
     arranged_problems = ''
     max_len = []
@@ -91,6 +81,18 @@ def arithmetic_arranger(problems, b = False):
     return arranged_problems
 
 
-#test 
+# mini test
 res = arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True)
 print (res);
+
+
+# arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+
+#   32         1      9999      523
+# +  8    - 3801    + 9999    -  49
+# ----    ------    ------    -----
+#   40     -3800     19998      474
+
+
+# code submitted in:
+# https://replit.com/@nahuelcastro/boilerplate-arithmetic-formatter#arithmetic_arranger.py
